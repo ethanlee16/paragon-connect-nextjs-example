e8a9fe335d199a1174cc533c7d50253dec075b18
Ethan Lee <ethan@useparagon.com>
2023-09-08 21:14:20 +0000

    Update shopify.js

--- pages/integrations/shopify.js
+++ pages/integrations/shopify.js
@@ -3,6 +3,9 @@ import { useRouter } from 'next/router';
 
 import useParagon from "../../hooks/useParagon";
 
+// The URL of your Pipedrive Redirect Page
+const SHOPIFY_REDIRECT_URL = "https://w4tdtq-3000.csb.app/integrations/shopify";
+
 export default function InstallShopify({ paragonUserToken }) {
   const { user, paragon } = useParagon(paragonUserToken);
   const router = useRouter();
@@ -10,17 +13,22 @@ export default function InstallShopify({ paragonUserToken }) {
   useEffect(() => {
     if (window.location.search && paragon) {
       let params = new URLSearchParams(window.location.search);
-      if (params.get("code")) {
+      let authorizationCode = params.get("code");
+      let [, shopName] = params.get("shop").match(/^([a-zA-Z0-9][a-zA-Z0-9\-]*)\.myshopify\.com/);
+
+      if (authorizationCode && shopName) {
          paragon.completeInstall("shopify", {
-          authorizationCode: params.get("code"),
-          redirectUrl: "https://w4tdtq-3000.csb.app/integrations/shopify",
+          authorizationCode: authorizationCode,
+          redirectUrl: SHOPIFY_REDIRECT_URL,
           integrationOptions: {
-            SHOP_NAME: params.get("shop").split(".myshopify.com")[0],
-          },
-          showPortalAfterInstall: true
+            SHOP_NAME: shopName,
+          }
         }).then(() => {
            router.push('/integrations?shopify_install=true');
         });
+      } else {
+        let error = params.get("error");
+        // Handle error
       }
     }
   }, [paragon]);
